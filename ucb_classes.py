187912f4c65b532d3d43213a323ff3a6c05294de
NICOLAS SCHREUDER <schreuder.nicolas@gmail.com>
2019-03-07 20:53:13 +0100

    Improved UCB algorithms code

--- ucb_classes.py
+++ ucb_classes.py
@@ -3,17 +3,20 @@ import numpy as np
 import bisect
 
 class lilUCB():
-    def __init__(self, δ, ϵ, β, K):
+    def __init__(self, δ, ϵ, β, K, σ=.5):
         """
         Initialize parameters
         """
         self.ϵ = ϵ
         self.β = β
         self.λ = ((2+self.β)/self.β)**2
-        self.K = K
-        self.σ = .5
+        self.K = K # number of arms
+        self.σ = σ
+        
+        # upper bound constant
         self.UCB_cst = 2*(self.σ**2)*(1+self.ϵ)
         
+        # confidence for given confidence δ
         self.δ = np.log(1+self.ϵ)*(δ*self.ϵ/(2+self.ϵ))**(1/(1+self.ϵ))
         
     def stopping_criterion(self, T, sum_T):
@@ -21,7 +24,7 @@ class lilUCB():
     
     def UCB(self, T):
         """
-        Compute upper confidence bound for each arm
+        Compute upper confidence bounds for the arms
         """
         lil = np.log(np.log((1+self.ϵ)*T)/self.δ)
         return np.sqrt(self.UCB_cst*lil/T)
@@ -31,16 +34,23 @@ class lilUCB():
         T = np.zeros(self.K) # number of pulls per arm
         sum_T = 0 # total number of pulls
         theta_hat = np.zeros(self.K) # estimation of parameter theta
-        for k in range(self.K):
-            theta_hat[k]  = mab.pull_arm(k)
-            T[k] +=1
-            sum_T+=1
         
-        UCB_values = theta_hat + (1+self.β)*(1+np.sqrt(self.ϵ))*self.UCB(T)        
-        while not self.stopping_criterion(T, sum_T):    
+        # initialize by pulling each arm once
+        for k in range(self.K):
+            theta_hat[k]  = mab.pull_arm(k) 
+            T[k] += 1
+            sum_T += 1
+        
+        # compute upper confidence bounds
+        UCB_values = theta_hat + (1+self.β)*(1+np.sqrt(self.ϵ))*self.UCB(T)     
+        
+        while not self.stopping_criterion(T, sum_T): 
+            # find and pull arm with maximum upper confidence bound
             I = np.argmax(UCB_values)
             
             reward = mab.pull_arm(I)
+            
+            # update estimation of paper for chosen arm
             theta_hat[I] = (T[I]*theta_hat[I] + reward)/(T[I]+1)    
             
             # update counters
@@ -54,22 +64,28 @@ class lilUCB():
 #lilUCB_ = lilUCB(δ=0.1, ϵ=0.01, β=1, K=20)
 
 class robust_lilUCB():
-    def __init__(self, δ, ϵ, β, K, theoretical_cst=False, n0=1):
+    def __init__(self, δ, ϵ, β, K, σ=.5, theoretical_cst=False, n0=1):
         self.ϵ = ϵ
         self.β = β
         self.λ = ((2+self.β)/self.β)**2
         self.K = K
-        self.σ = .5
+        self.σ = σ
+        
+        # strong convexity radius
         self.r = 0.5
-        #self.α = np.exp(-0.5/self.σ**2)/np.sqrt(2*np.pi*self.σ**2)
+        
+        # strong convexity constant
         self.α = 1
         
+        # warm-up index
         #self.n_0 = ((32*(1+self.ϵ)*self.σ**2)/(self.r**2 * self.α**2)
         #            *np.log(2*np.log((32*(1+self.ϵ)*self.σ**2)/(self.r**2 * self.α**2 * self.δ))/self.δ))
         self.n0 = n0
         
+        # algorithm confidence for given final confidence
         self.δ = np.log(1+ϵ)**(1+ϵ)/(4*(1+1/ϵ))*δ
         
+        # True if bounds are computed with theoretical constants
         self.theoretical_cst = theoretical_cst
         
     def stopping_criterion(self, T, sum_T):
@@ -79,13 +95,13 @@ class robust_lilUCB():
         """
         Compute upper confidence bound for each arm
         """
-        lil = np.log(np.log(T)/self.δ)
+        lil = (1+self.ϵ)*np.log(np.log(T)) + np.log(1/self.δ)
         
         if self.theoretical_cst==True:
-            cst = 32*(1+self.ϵ)*self.σ**2/self.α**2
+            cst = 4*np.sqrt(2)*self.σ/self.α
         else:
             cst = 1
-        return np.sqrt(cst*lil/T)
+        return cst*np.sqrt(lil/T)
     
     def run(self, mab):
         # initialization
