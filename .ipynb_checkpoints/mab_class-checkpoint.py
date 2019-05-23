78f60d9cea6e7ae87f9273ff9713ee0b5eef4795
SCHREUDER Nicolas <nschreuder@labostat.ensae.fr>
2019-05-23 12:24:58 +0200

    Commit before submission

--- .ipynb_checkpoints/mab_class-checkpoint.py
+++ .ipynb_checkpoints/mab_class-checkpoint.py
@@ -1,11 +1,12 @@
 import numpy as np
+from scipy.stats import t
 
 class MAB():
     """
     Stochastic multi armed bandit framework
     """
     
-    def __init__(self, K, scenario, distrib, σ=0.5, α=0):
+    def __init__(self, K, scenario, distrib, σ=.5, α=0):
         """
         Initialize multi-armed bandit game
         K : int, number of arms
@@ -17,9 +18,7 @@ class MAB():
         self.distrib = distrib
         self.α = α
         self.σ = σ
-        
-        np.random.seed(np.random.randint(0, 500))
-        
+                        
         # initialize parameter theta
         if scenario == 'sparse':
             # sparse model
@@ -32,19 +31,33 @@ class MAB():
             self.θ = np.ones(K)
             for k in range(1, K):
                 self.θ[k] = self.θ[k] - (k/K)**self.α
-    
+                
     def pull_arm(self, k):
         """
         Pull arm k and receive reward
         k : int, pulled arm index
         """
         #assert k < self.K # valid arm
-        
+                
         if self.distrib == "cauchy":
             reward =  self.θ[k] + np.random.standard_cauchy()
+        
+        elif self.distrib == "student3":
+            reward = self.θ[k] + self.σ*np.random.standard_t(df=3)/np.sqrt(3)
+        
+        elif self.distrib == "student2":
+            reward = self.θ[k] + np.random.standard_t(df=2)
+        
+        elif self.distrib == "laplace":
+            reward = np.random.laplace(loc=self.θ[k], scale=self.σ/np.sqrt(2))
             
         elif self.distrib == "gaussian":
             reward = np.random.normal(loc=self.θ[k], scale=self.σ)
+        
+        elif self.distrib == "cauchy_gaussian_mixture":
+            contamination = np.random.binomial(n=1, p=0.1)
+            reward = (self.θ[k] + (1-contamination)*np.random.normal(scale=self.σ) 
+                      + contamination*np.random.standard_cauchy())
         
         elif self.distrib == "huber":
             binomial_draw = np.random.binomial(n=1, p=0.1)
--- ucb_classes.py
+++ ucb_classes.py
@@ -3,21 +3,22 @@ import numpy as np
 import bisect
 
 class lilUCB():
-    def __init__(self, K, δ=.1, ϵ=.01, β=1., σ=.5):
+    def __init__(self, K, ν=.1, ɛ=.01, β=1., σ=1.):
         """
         Initialize parameters
         """
-        self.ϵ = ϵ
+        self.ɛ = ɛ
         self.β = β
         self.λ = ((2+self.β)/self.β)**2
         self.K = K # number of arms
         self.σ = σ
         
         # upper bound constant
-        self.UCB_cst = 2*(self.σ**2)*(1+self.ϵ)
+        self.UCB_cst = (1+β)*(1+np.sqrt(self.ɛ))*self.σ*np.sqrt(2*(1+self.ɛ))
         
         # confidence for given confidence δ
-        self.δ = np.log(1+self.ϵ)*(δ*self.ϵ/(2+self.ϵ))**(1/(1+self.ϵ))
+        self.c_ɛ = (2+ɛ)*(1/np.log(1+ɛ))**(1+ɛ)/ɛ
+        self.δ = (np.sqrt(1+ν)-1)**2/(4*self.c_ɛ)
         
     def stopping_criterion(self, T, sum_T):
         return ((1+self.λ)*T >= 1 + self.λ*sum_T).any()
@@ -26,10 +27,11 @@ class lilUCB():
         """
         Compute upper confidence bounds for the arms
         """
-        lil = np.log(np.log((1+self.ϵ)*T)/self.δ)
-        return np.sqrt(self.UCB_cst*lil/T)
+        lil = np.log(np.log((1+self.ɛ)*T)/self.δ)
+        return self.UCB_cst*np.sqrt(lil/T)
     
-    def run(self, mab):        
+    def run(self, mab): 
+                
         # initialization
         T = np.zeros(self.K) # number of pulls per arm
         sum_T = 0 # total number of pulls
@@ -42,9 +44,9 @@ class lilUCB():
             sum_T += 1
         
         # compute upper confidence bounds
-        UCB_values = theta_hat + (1+self.β)*(1+np.sqrt(self.ϵ))*self.UCB(T)     
+        UCB_values = theta_hat + self.UCB(T)     
         
-        while not self.stopping_criterion(T, sum_T) and (sum_T < 5000*self.K) :
+        while not self.stopping_criterion(T, sum_T):
             # find and pull arm with maximum upper confidence bound
             I = np.argmax(UCB_values)
             
@@ -56,40 +58,37 @@ class lilUCB():
             # update counters
             T[I] += 1
             sum_T += 1
-            UCB_values[I] = theta_hat[I] + (1+self.β)*(1+np.sqrt(self.ϵ))*self.UCB(T[I])
+            UCB_values[I] = theta_hat[I] + self.UCB(T[I])
+            
             
         return T
     
-#a = MAB(20, 'sparse', False, 0.3)
-#lilUCB_ = lilUCB(δ=0.1, ϵ=0.01, β=1, K=20)
 
 class robust_lilUCB():
-    def __init__(self, K, δ=.1, ϵ=.01, β=1., σ=.5, theoretical_cst=False, n0=1):
-        self.ϵ = ϵ
+    def __init__(self, K, ν=.1, β=1., σ=1.):
         self.β = β
         self.λ = ((2+self.β)/self.β)**2
         self.K = K
         self.σ = σ
         
         # strong convexity radius
-        self.r = 0.5
+        self.r = .5
         
         # strong convexity constant
-        self.α = 0.5
+        self.α = 0.97
+        
+        # algorithm confidence
+        self.δ = ((np.sqrt(11*ν+9)-3)/11)**2
+                
+        self.c_δ = 0.72*np.log(10.4/self.δ)
+        self.ucb_constant = (1+self.β)*3.4*self.σ/self.α
         
         # warm-up index
-        self.n0 = 200
-        
-        # algorithm confidence for given final confidence
-        #self.δ = np.log(1+ϵ)**(1+ϵ)/(4*(1+1/ϵ))*δ
-        self.δ = δ
-        
-        self.c_δ = 0.72*np.log(5.2/δ)
-        self.ucb_constant = (1+self.β)*3.4*self.σ/self.α
+        self.n_0 = 423
                 
     def stopping_criterion(self, T, sum_T):
-        return ((1+self.λ)*T >= 1 + self.λ*sum_T).any()
-    
+        return ((1+self.λ)*T >= self.n_0 + self.λ*sum_T).any()
+        
     def upper_bound(self, T):
         """
         Compute upper confidence bound for each arm
@@ -104,11 +103,11 @@ class robust_lilUCB():
         theta_hat = [[] for k in range(self.K)]
         for k in range(self.K):
             theta_hat[k]  = [mab.pull_arm(k)]
-            T[k] +=1
+            T[k] += 1
             sum_T += 1
         
-        # pull each arm n0 times
-        for l in range(self.n0):
+        # pull each arm n_0 times
+        for l in range(self.n_0):
             for k in range(self.K):
                 reward = mab.pull_arm(k)
                 bisect.insort(theta_hat[k], reward)
@@ -133,113 +132,3 @@ class robust_lilUCB():
             
         return T
     
-#a = MAB(20, 'sparse', False, 0.3)
-#lilUCB_ = lilUCB(δ=0.1, ϵ=0.01, β=1, K=20)
-
-
-class medianUCB():
-    def __init__(self, δ, ϵ, K):
-        self.δ = δ
-        self.ϵ = ϵ
-        self.K = K
-        self.σ = 1.
-        self.ρ = 0.3
-        
-    def stopping_criterion(self, T, medians):
-        BAI = np.argmax(medians)
-        B = self.UCB(T)
-        for k in range(self.K):
-            if k==BAI:
-                continue
-            if medians[BAI] - B[BAI] < medians[k]  + B[k]:
-                return False
-        return True
-    
-    def UCB(self, T):
-        """
-        Compute upper confidence bound for each arm
-        """
-        return np.sqrt(np.log(T/self.δ)/(2*T))
-    
-    def lil_upper_bound(self, T):
-        """
-        Compute upper confidence bound for each arm
-        """
-        lil = np.log(np.log((1+self.ϵ)*T/self.δ)/self.δ)
-        cst = 2*(self.σ**2)*(1+self.ϵ)
-        return np.sqrt(cst*lil/T)
-
-    
-    def run(self, mab):
-        # initialization
-        n = 0
-        T = np.zeros(self.K, dtype=int)
-        theta_hat = [[] for k in range(self.K)]
-        for k in range(self.K):
-            theta_hat[k]  += [mab.pull_arm(k)]
-            T[k] +=1
-            n+=1
-        
-        medians = np.array([theta_hat[k][T[k]//2] for k in range(self.K)])
-        
-        while not self.stopping_criterion(T, medians):
-            if n>1e5*(4*self.K):
-                return T
-            # find max UCB index
-            medians = np.array([theta_hat[k][T[k]//2] for k in range(self.K)])
-            I = np.argmax(medians + self.ρ*self.UCB(T))
-            reward = mab.pull_arm(I)
-            bisect.insort(theta_hat[I], reward)
-            
-            # update counters
-            T[I] += 1
-            n+=1
-        return T
-    
-class nonadaptive():
-    def __init__(self, K, δ, ϵ):
-        self.K = K
-        self.δ = δ
-        self.ϵ = ϵ
-        self.σ = .3
-        
-    def UCB(self, T):
-        # self.K is a typo ?
-        LIL = np.log(2*np.log((1+self.ϵ)*T + 2)/self.δ)
-        sqrt_cst = 2*self.σ**2*(1+self.ϵ)
-        cst = 1+np.sqrt(self.ϵ)
-        return cst*np.sqrt(sqrt_cst*LIL/T)
-    
-    def stopping_criterion(self, theta_hat, T):
-        BAI = np.argmax(theta_hat)
-        B = self.UCB(T)
-        for k in range(self.K):
-            if k==BAI:
-                continue
-            
-            if theta_hat[BAI] - B[BAI] < theta_hat[k]  + B[k]:
-                return False
-        return True
-    
-    def run(self, mab):
-        
-        self.cycle = np.random.permutation(np.arange(10))
-        
-        # initialization
-        n = 0
-        T = np.ones(self.K)
-        theta_hat = np.zeros(self.K)
-        for k in self.cycle:
-            theta_hat[k]  = mab.pull_arm(k)
-            n+=1
-        while not self.stopping_criterion(theta_hat, T):
-            for k in self.cycle:
-                reward = mab.pull_arm(k)
-                theta_hat[k]  = (T[k]*theta_hat[k] + reward)/(T[k] + 1)
-                T[k] += 1
-                n+=1
-        
-        return T, theta_hat
-    
-# nonadaptive_proc = nonadaptive(K=10, δ=0.2, ϵ=0.01)
-# T = nonadaptive_proc.run(a)
